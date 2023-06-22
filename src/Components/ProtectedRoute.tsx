59e656040b035f3c6869d3f06604cb82d863876b
Nicolas Ayan <nicolas.ayan18'@'gmail.com>
2023-06-22 20:28:28 -0300

    fixed

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import { type PropsProtectedRoute } from '../Types'
 
-export default function ProtectedRoute ({ children, isAllowed, message}: PropsProtectedRoute): JSX.Element {
+export default function ProtectedRoute ({ children, isAllowed, message }: PropsProtectedRoute): JSX.Element {
   if (!isAllowed) {
     return (message)
   }
